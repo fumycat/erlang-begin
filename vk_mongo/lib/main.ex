99dd586538e001743e45e9600366793d86fcb608
Vladislav <fumycat@gmail.com>
2021-08-11 13:13:06 +0700

    handle vk errors

--- vk_mongo/lib/main.ex
+++ vk_mongo/lib/main.ex
@@ -4,35 +4,35 @@ defmodule Main do
   """
   @api "https://api.vk.com/method/"
 
-  defp call_vk(method, args) do
+  defp form_request(method, args) do
     args_list = for {k, v} <- args, do: "#{k}=#{v}"
 
-    request =
-      @api <>
-        method <>
-        "?" <> Enum.join(args_list, "&") <> "&access_token=" <> System.fetch_env!("VK_TOKEN")
-
-    {:ok, request}
+    @api <> method <> "?" <> Enum.join(args_list, "&") <> "&access_token=" <> System.fetch_env!("VK_TOKEN")
   end
 
   def users_get(user_ids) do
-    {:ok, s} = call_vk("users.get", %{user_ids: Enum.join(user_ids, ","), v: "5.131"})
-    s
+    form_request("users.get", %{user_ids: Enum.join(user_ids, ","), v: "5.131"})
   end
 
-  def mongo do
+  def start do
     {:ok, conn} = Mongo.start_link(url: "mongodb://localhost:27017/vk")
     Mongo.insert_many(conn, "users", test_users(), [])
-    :ok
   end
 
   def test_users do
     :inets.start()
 
-    {:ok, {_status, _headers, body}} =
-      users_get(["e1iana", "doombar", "salobur", "tanya_lukasevich", "innerstate"])
-      |> :httpc.request()
+    request_str = users_get(["e1iana", "doombar", "salobur", "tanya_lukasevich", "innerstate"])
+    {:ok, {_status, _headers, body}} = :httpc.request(request_str)
 
-    JSON.decode!(body) |> Map.get("response")
+    case body |> JSON.decode!() do
+      r when is_map_key(r, "response") ->
+        Map.get(r, "response")
+
+      r ->
+        IO.inspect(r)
+        raise "VK Error"
+    end
+
   end
 end
